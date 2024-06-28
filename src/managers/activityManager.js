6641f9474c6a9f3686a37108e90442b424124faf
tim-pearson <timothy-edward.pearson@epita.fr>
2024-06-29 01:14:39 +0200

    feat: python word list

--- src/managers/activityManager.js
+++ src/managers/activityManager.js
@@ -1,10 +1,19 @@
 import { useState, useEffect, createContext, useRef } from "react";
+import words from "../../public/words.json";
 
 export const setupActivityManager = () => {
     const [xpBoost, setXpBoost] = useState(0);
     const [xp, setXp] = useState(50);
     const [keySequence, setKeySequence] = useState([]);
     const lastActivityTimeRef = useRef(Date.now());
+
+    const wordWeight = 3;
+    const inactiveWeight = 3;
+    const maxWeight = 5;
+    const boostInc = 1
+    const nbImages = 6;
+    const inactivityLimit = 10000;
+    const boostPeriod = 500; 
 
     const handleActivity = () => {
         lastActivityTimeRef.current = Date.now();
@@ -13,29 +22,31 @@ export const setupActivityManager = () => {
     let curXpBoost = xpBoost;
     let curXp = xp;
     const updateBoost = (inc) => {
-        curXpBoost += inc ? 1 : -1;
-        curXpBoost = Math.max(-2, Math.min(curXpBoost, 5));
-        // if (curXpBoost < 0) { // inactive
-        //     curXp = Math.max(0, curXp + curXpBoost);
-        //     curXp = Math.max(0, Math.min(600, curXp)); // 600 because 6 images
-        //     setXp(curXp);
-        // }
+        curXpBoost += inc ? boostInc : -boostInc;
+        curXpBoost = Math.max(-1, Math.min(curXpBoost, maxWeight));
+        if (curXpBoost < 0) { // inactive
+            curXp = curXp + inactiveWeight;
+            curXp = Math.max(0,  curXp); 
+            setXp(curXp);
+        }
         setXpBoost(curXpBoost);
 
     };
-    const updateXp = () => {
-        curXp += 20;
-        curXp = Math.max(0, Math.min(600, curXp)); // 600 because 6 images
+    const updateXp = (weight) => {
+        curXp += weight * curXpBoost;
+        curXp = Math.min(nbImages * 100, curXp); 
         setXp(curXp);
     };
-    useEffect(() => {
+
+      useEffect(() => {
         const handleKeyPress = (event) => {
             const key = event.key;
             setKeySequence(prevSequence => {
-                const newSequence = [...prevSequence, key].slice(-4);
-                if (newSequence.join('') === 'test') {
-                    updateXp();
-                    return [];
+                const newSequence = [...prevSequence, key].slice(-Math.max(...words.map(seq => seq.length)));
+                const matchedSequence = words.find(seq => newSequence.join('').endsWith(seq));
+                if (matchedSequence) {
+                    updateXp(wordWeight);
+                    return []; 
                 }
                 return newSequence;
             });
@@ -46,15 +57,14 @@ export const setupActivityManager = () => {
         return () => {
             document.removeEventListener('keypress', handleKeyPress);
         };
-    }, []);
-    
+    }, []);   
     useEffect(() => {
         const interval = setInterval(() => {
             const currentTime = Date.now();
             const timeSinceLastActivity = currentTime - lastActivityTimeRef.current;
-            const inc = timeSinceLastActivity < 10000;
+            const inc = timeSinceLastActivity < inactivityLimit;
             updateBoost(inc);
-        }, 500); // change this time interval for faster/slower progress
+        }, boostPeriod);
 
         return () => {
             clearInterval(interval);
